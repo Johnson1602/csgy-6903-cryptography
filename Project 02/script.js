96addebd72d6b47fa68b1c654d4d7ff1669a1051
Weiyi Xu <johnsonxwy@gmail.com>
2020-12-06 16:06:21 -0500

    update front-end design

--- Project 02/script.js
+++ Project 02/script.js
@@ -2,7 +2,6 @@ const questionData = {
 	scene: {
 		question: "What do you want to use the scheme for?",
 		answer: ["Application", "Study"],
-		prompt: "I want to use the scheme for ",
 		func: function (that, result) {
 			// that: the clicked button (with its value: that.value)
 			// result:
@@ -10,7 +9,7 @@ const questionData = {
 				// hide button (choices)
 				that.parentNode.style.display = "none"
 				// show choosen result prompt
-				that.parentNode.nextSibling.style.display = "block"
+				that.parentNode.previousSibling.querySelector(".result").style.display = "block"
 				creationQuestion(questionData["device"], that.value)
 			} else {
 				let result = document.createElement("p")
@@ -22,11 +21,10 @@ const questionData = {
 	device: {
 		question: "What device are you planning to use?",
 		answer: ["Normal Device", "Resource Constrained Device"],
-		prompt: "I want to use it on ",
 		func: function (that, result) {
 			if (that.value == "Normal Device") {
 				that.parentNode.style.display = "none"
-				that.parentNode.nextSibling.style.display = "block"
+				that.parentNode.previousSibling.querySelector(".result").style.display = "block"
 				creationQuestion(questionData["feature"], that.value)
 			} else {
 				let result = document.createElement("p")
@@ -38,23 +36,21 @@ const questionData = {
 	feature: {
 		question: "What feature(s) is(are) the most important to you?",
 		answer: ["Security", "Efficiency", "Security And Efficiency"],
-		prompt: "I need ",
 		func: function (that, result) {
 			if (that.value == "Security And Efficiency") {
 				that.parentNode.style.display = "none"
-				that.parentNode.nextSibling.style.display = "block"
+				that.parentNode.previousSibling.querySelector(".result").style.display = "block"
 				creationQuestion(questionData["stream"], "Both")
 			} else {
 				that.parentNode.style.display = "none"
-				that.parentNode.nextSibling.style.display = "block"
+				that.parentNode.previousSibling.querySelector(".result").style.display = "block"
 				creationQuestion(questionData["hash"], that.value)
 			}
 		}
 	},
 	hash: {
-		question: "Do you need this scheme to work as part of a hash function?",
-		answer: ["YES", "NO"],
-		prompt: "",
+		question: "Do you need this scheme to work with hash function?",
+		answer: ["YES", "NO", "I'm Not Sure"],
 		func: function (that, result) {
 			// come from feature needs security only
 			if (result == "Security") {
@@ -64,7 +60,7 @@ const questionData = {
 					main.appendChild(result)
 				} else {
 					that.parentNode.style.display = "none"
-					that.parentNode.nextSibling.style.display = "block"
+					that.parentNode.previousSibling.querySelector(".result").style.display = "block"
 					creationQuestion(questionData["stream"], "Security")
 				}
 			} else if (result == "Efficiency") {
@@ -74,7 +70,7 @@ const questionData = {
 					main.appendChild(result)
 				} else {
 					that.parentNode.style.display = "none"
-					that.parentNode.nextSibling.style.display = "block"
+					that.parentNode.previousSibling.querySelector(".result").style.display = "block"
 					creationQuestion(questionData["stream"], "Efficiency")
 				}
 			} else if (result == "Both") {
@@ -93,7 +89,6 @@ const questionData = {
 	stream: {
 		question: "Do you need this scheme to encrypt streaming data?",
 		answer: ["YES", "NO"],
-		prompt: "",
 		func: function (that, result) {
 			if (result == "Security") {
 				if (that.value == "YES") {
@@ -102,7 +97,7 @@ const questionData = {
 					main.appendChild(result)
 				} else {
 					that.parentNode.style.display = "none"
-					that.parentNode.nextSibling.style.display = "block"
+					that.parentNode.previousSibling.querySelector(".result").style.display = "block"
 					creationQuestion(questionData["photo"], "Security")
 				}
 			} else if (result == "Efficiency") {
@@ -112,7 +107,7 @@ const questionData = {
 					main.appendChild(result)
 				} else {
 					that.parentNode.style.display = "none"
-					that.parentNode.nextSibling.style.display = "block"
+					that.parentNode.previousSibling.querySelector(".result").style.display = "block"
 					creationQuestion(questionData["photo"], "Efficiency")
 				}
 			} else if (result == "Both") {
@@ -122,16 +117,15 @@ const questionData = {
 					main.appendChild(result)
 				} else {
 					that.parentNode.style.display = "none"
-					that.parentNode.nextSibling.style.display = "block"
+					that.parentNode.previousSibling.querySelector(".result").style.display = "block"
 					creationQuestion(questionData["photo"], "Both")
 				}
 			}
 		}
 	},
 	photo: {
-		question: "Are you planning to ecrypt photo files using this scheme?",
+		question: "Do you need this scheme to encrypt photo?",
 		answer: ["YES", "NO"],
-		prompt: "",
 		func: function (that, result) {
 			if (result == "Security") {
 				if (that.value == "YES") {
@@ -160,7 +154,7 @@ const questionData = {
 					main.appendChild(result)
 				} else {
 					that.parentNode.style.display = "none"
-					that.parentNode.nextSibling.style.display = "block"
+					that.parentNode.previousSibling.querySelector(".result").style.display = "block"
 					creationQuestion(questionData["hash"], "Both")
 				}
 			}
@@ -171,7 +165,7 @@ const questionData = {
 var currentIndex = 0
 
 // main questions section
-var main = document.querySelector(".main")
+const main = document.querySelector(".main")
 
 function creationQuestion(currentQuestion, result) {
 	// question wrapper
@@ -180,11 +174,58 @@ function creationQuestion(currentQuestion, result) {
 	questionWrapper.setAttribute("index", currentIndex)
 	currentIndex++
 
-	// actual question
+	// qhead
+	let qhead = document.createElement("div")
+	qhead.className = "qhead"
+
+	// actual question h4
 	let h4 = document.createElement("h4")
 	h4.className = "question"
 	h4.innerHTML = currentQuestion.question
-	questionWrapper.appendChild(h4)
+	qhead.appendChild(h4)
+
+	// result button
+	let resultWrapper = document.createElement("div")
+	resultWrapper.className = "result"
+	let resultButton = document.createElement("button")
+	let span = document.createElement("span")
+	span.innerHTML = "Click to &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;change"
+	resultButton.addEventListener("click", function () {
+		this.parentNode.parentNode.parentNode.className = "question-wrapper"
+		// remove all the questions after current question
+		let questions = document.querySelectorAll(".question-wrapper")
+		index = Number(this.parentNode.parentNode.parentNode.getAttribute("index")) + 1
+		for (i = index; i < currentIndex; i++) {
+			questions[i].remove()
+		}
+		currentIndex = index
+		// hide result & show choice again
+		this.parentNode.style.display = "none"
+		this.parentNode.parentNode.nextSibling.style.display = "block"
+
+		// current button's question's offset top
+		let currentOffsetTop = this.parentNode.parentNode.parentNode.offsetTop
+		// window height
+		let windowHeight = window.innerHeight
+		// optimal height
+		let optimalHeight = Math.floor(windowHeight / 2 - 100)
+
+		// if new question is too low in window, adjust window position
+		if (currentOffsetTop < optimalHeight) {
+			animate(window, 0)
+		}
+		// else {
+		// 	// calculate scroll distance
+		// 	let distance = Math.floor(currentOffsetTop - optimalHeight)
+		// 	// let distance = Math.floor(newOffsetTop - optimalHeight) + 40
+		// 	main.style.height = windowHeight + distance - 120 + "px"
+		// 	animate(window, distance)
+		// }
+	})
+	resultWrapper.appendChild(resultButton)
+	resultWrapper.appendChild(span)
+	qhead.appendChild(resultWrapper)
+	questionWrapper.appendChild(qhead)
 
 	// answer wrapper
 	let answerWrapper = document.createElement("div")
@@ -201,39 +242,56 @@ function creationQuestion(currentQuestion, result) {
 		// register events for buttons
 		button.addEventListener("click", function () {
 			that = this
-			this.parentNode.nextSibling.querySelector("a").innerHTML = this.value
+			this.parentNode.parentNode.className = "question-wrapper selected"
+			this.parentNode.previousSibling.querySelector("button").innerHTML = this.value
 			currentQuestion.func(that, result)
+			// current button's question's offset top
+			let currentOffsetTop = this.parentNode.parentNode.offsetTop
+			// new question's offset top
+			let newOffsetTop = currentOffsetTop + 90
+			// window height
+			let windowHeight = window.innerHeight
+			// optimal height
+			let optimalHeight = Math.floor(windowHeight / 2 - 100)
+
+			// if new question is too low in window, adjust window position
+			if (newOffsetTop > optimalHeight) {
+				// calculate scroll distance
+				let distance = Math.floor(newOffsetTop - optimalHeight)
+				// let distance = Math.floor(newOffsetTop - optimalHeight) + 40
+				main.style.height = windowHeight + distance - 120 + "px"
+				animate(window, distance)
+			}
 		})
 		answerWrapper.appendChild(button)
 	}
 	questionWrapper.appendChild(answerWrapper)
 
-	// result
-	let resultWrapper = document.createElement("div")
-	resultWrapper.className = "result"
-	resultWrapper.style.display = "none"
-	let prompt = document.createElement("span")
-	prompt.innerHTML = currentQuestion.prompt
-	let a = document.createElement("a")
-	a.href = "javascript:void(0)"
-	a.addEventListener("click", function () {
-		// remove all the questions after current question
-		let questions = document.querySelectorAll(".question-wrapper")
-		index = Number(this.parentNode.parentNode.getAttribute("index")) + 1
-		for (i = index; i < currentIndex; i++) {
-			questions[i].remove()
-		}
-		currentIndex = index
-		// hide result & show choice again
-		this.parentNode.style.display = "none"
-		this.parentNode.previousSibling.style.display = "block"
-	})
-	resultWrapper.appendChild(prompt)
-	resultWrapper.appendChild(a)
-	questionWrapper.appendChild(resultWrapper)
-
 	// add new question to page
 	main.appendChild(questionWrapper)
+}
+
+// animation
+function animate(obj, targetPosition, callback) {
+	clearInterval(obj.timer);
+
+	obj.timer = setInterval(function () {
+		console.log("still running");
+		var offsetTop = window.pageYOffset;
+		// console.log("offsetTop: " + offsetTop);
+		// console.log("target: " + targetPosition);
+		var step = (targetPosition - offsetTop) / 10;
+		step = step > 0 ? Math.ceil(step) : Math.floor(step);
+		if (offsetTop == targetPosition) {
+			clearInterval(obj.timer);
+			console.log("finish");
+			if (callback) {
+				callback();
+			}
+		} else {
+			window.scroll(0, offsetTop + step);
+		}
+	}, 10)
 }
 
 creationQuestion(questionData["scene"], "")
