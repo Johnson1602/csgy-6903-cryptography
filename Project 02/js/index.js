1682cd2c35fd8394be8cb20cbf9ec8e10e6f596b
Ryan_Wang <zw2315@nyu.edu>
2020-12-07 21:16:57 -0500

    add scheme table

--- Project 02/js/index.js
+++ Project 02/js/index.js
@@ -3,9 +3,49 @@ const schemeData = [{
 	standardization: "Was a US standard",
 	efficiency: "Was fast, now slower than AES",
 	security: "Breakable via efficient exhaustive key-search attack",
-	block: "Short for hash function uses",
+	block: "64 bits, short for hash function uses",
 	key: "56 bits (+8 parity bits)",
-	ram: "not quiet sure"
+	type: "Feistel"
+},{
+	scheme: "Triple DES",
+	standardization: "Based on DES which was a standard",
+	efficiency: "Slower than DES and AES",
+	security: "Not known to be efficiently breakable",
+	block: "64 bits, short for hash function uses",
+	key: "168 bits (three 56-bit DES keys)",
+	type: "Feistel"
+},{
+	scheme: "Blowfish",
+	standardization: "Intended as an alternative to the aging DES",
+	efficiency: "A fast cipher that faster than AES",
+	security: " Vulnerable to birthday attack",
+	block: "64 bits, short for hash function uses",
+	key: "32 – 448 bits",
+	type: "Feistel"
+},{
+	scheme: "AES",
+	standardization: "Current US standard",
+	efficiency: "Fast enough for almost all applications(except for resource-constrained devices)",
+	security: "Not known to be efficiently breakable",
+	block: "128 bits, large enough for hash function uses",
+	key: "128, 192, or 256 bits",
+	type: "SPN"
+},{
+	scheme: "IDEA",
+	standardization: "Proposed as a DES replacement. Patents were issued for it.",
+	efficiency: "Fast enough for almost all applications(except for resource-constrained devices)",
+	security: "Not known to be efficiently breakable",
+	block: "64 bits, large enough for hash function uses",
+	key: "128 bits",
+	type: "ARX"
+},{
+	scheme: "SIMON",
+	standardization: "Wannabe standard",
+	efficiency: "Faster than AES, targeting usage on resource-constrained devices",
+	security: "Breakable with large-computation attack",
+	block: "Short (32) to large (128)",
+	key: "64, 72, 96, 128, 144, 192 or 256 bits",
+	type: "Feistel"
 }]
 
 const tbody = document.querySelector('tbody');
